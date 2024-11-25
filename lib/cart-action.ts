df88557e70cfdb1f6f9594d26da53e86535da2c1
Guinn <eguinjonathan08@gmail.com>
2024-11-25 14:07:59 +0700

    Add quantity management functions for cart: increase, decrease, and remove items by user ID

--- lib/cart-action.ts
+++ lib/cart-action.ts
@@ -22,3 +22,54 @@ export const removeFromCart = async (id: string) => {
   });
   return cart;
 };
+
+export const decreaseQuantity = async (id: string) => {
+  const cart = await prisma.cart.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (cart?.quantity === 1) {
+    return await prisma.cart.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  const updateCart = await prisma.cart.update({
+    where: {
+      id,
+    },
+    data: {
+      quantity: {
+        decrement: 1,
+      },
+    },
+  });
+  return updateCart;
+};
+
+export const increaseQuantity = async (id: string) => {
+  const cart = await prisma.cart.update({
+    where: {
+      id,
+    },
+    data: {
+      quantity: {
+        increment: 1,
+      },
+    },
+  });
+  return cart;
+};
+
+//!used for checkout
+export const removeFromCartByUserId = async (userId: string) => {
+  const cart = await prisma.cart.deleteMany({
+    where: {
+      userId,
+    },
+  });
+  return cart;
+};
