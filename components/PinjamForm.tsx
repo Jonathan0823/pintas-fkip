f1eb4ef988db9e10623dcd8287ba05514a98cf4f
Guinn <eguinjonathan08@gmail.com>
2024-11-27 14:15:56 +0700

    Refactor PinjamForm component: improve layout, enhance user input handling, and replace file upload button with a file input

--- components/PinjamForm.tsx
+++ components/PinjamForm.tsx
@@ -3,28 +3,33 @@ import { User } from "@/types/User";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
-export default function PinjamForm({ onClose, selected }: { onClose: () => void, selected: string[] }) {
-  const {data: session} = useSession();
+export default function PinjamForm({
+  onClose,
+  selected,
+}: {
+  onClose: () => void;
+  selected: string[];
+}) {
+  const { data: session } = useSession();
   const [startDate, setStartDate] = useState<string>("");
   const [endDate, setEndDate] = useState<string>("");
   const [user, setUser] = useState<User>();
   const [namaKegiatan, setNamaKegiatan] = useState<string>("");
 
-
-
   useEffect(() => {
     if (!session) {
-      return
+      return;
     }
     const fetchUser = async () => {
-     const res = await getCurrentUserInfo(session?.user?.email || "");
+      const res = await getCurrentUserInfo(session?.user?.email || "");
       setUser(res);
-    }
+    };
 
     fetchUser();
   }, [session]);
-  
 
   return (
     <div className="relative z-50 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
@@ -36,7 +41,7 @@ export default function PinjamForm({ onClose, selected }: { onClose: () => void,
           <IoMdArrowRoundBack />
         </div>
 
-        <div className="bg-[#8B2323] mt-14 text-white text-center w-full py-4 px-6">
+        <div className="bg-[#8B2323] mt-10 md:mt-14 text-white text-center w-full py-4 px-6">
           <h1 className="text-2xl font-semibold">ISI BIODATA</h1>
         </div>
         <p className="text-[#a17667] font-bold text-lg my-3">
@@ -44,15 +49,19 @@ export default function PinjamForm({ onClose, selected }: { onClose: () => void,
         </p>
         <div className="md:w-96 w-80 bg-white rounded-2xl border-2 border-black border-dashed flex flex-col max-w-md mx-4 shadow-lg">
           <div className="p-6 text-xs text-[#8B2323] font-bold">
-            <form className="space-y-4">
+            <form className="space-y-2 md:space-y-4">
               <div className="flex">
                 <label className="w-1/3 text-[#8B2323]">Nama</label>
                 <span className="w-8 text-center">:</span>
                 <input
                   type="text"
                   className="flex-1 border-b border-gray-300 focus:outline-none text-[#a17667] focus:border-[#8B2323]"
-                  value={user?.name|| ""}
-                  onChange={(e) => setUser((prev) => prev ? {...prev, name: e.target.value} : undefined)}
+                  value={user?.name || ""}
+                  onChange={(e) =>
+                    setUser((prev) =>
+                      prev ? { ...prev, name: e.target.value } : undefined
+                    )
+                  }
                 />
               </div>
               <div className="flex">
@@ -62,7 +71,11 @@ export default function PinjamForm({ onClose, selected }: { onClose: () => void,
                   type="text"
                   className="flex-1 border-b border-gray-300 focus:outline-none text-[#a17667] focus:border-[#8B2323]"
                   value={user?.namaormawa || ""}
-                  onChange={(e) => setUser((prev) => prev ? {...prev, namaormawa: e.target.value} : undefined)}
+                  onChange={(e) =>
+                    setUser((prev) =>
+                      prev ? { ...prev, namaormawa: e.target.value } : undefined
+                    )
+                  }
                 />
               </div>
               <div className="flex">
@@ -72,7 +85,11 @@ export default function PinjamForm({ onClose, selected }: { onClose: () => void,
                   type="tel"
                   className="flex-1 border-b border-gray-300 text-[#a17667] focus:outline-none focus:border-[#8B2323]"
                   value={user?.telepon || ""}
-                  onChange={(e) => setUser((prev) => prev ? {...prev, telepon: e.target.value} : undefined)}
+                  onChange={(e) =>
+                    setUser((prev) =>
+                      prev ? { ...prev, telepon: e.target.value } : undefined
+                    )
+                  }
                 />
               </div>
               <div className="flex">
@@ -109,7 +126,7 @@ export default function PinjamForm({ onClose, selected }: { onClose: () => void,
                 </div>
               </div>
               <div>
-                <p className="text-[#8B2323] text-[10px] mt-28">
+                <p className="text-[#8B2323] text-[10px] mt-10 md:mt-28">
                   Silahkan upload file surat peminjaman dibawah ini:
                 </p>
                 <p className="text-[#FF0000] text-[8px] mb-2">
@@ -119,13 +136,15 @@ export default function PinjamForm({ onClose, selected }: { onClose: () => void,
             </form>
           </div>
         </div>
-        <div className="px-6 py-6 mt-auto bg-black w-full">
-          <button
-            type="submit"
-            className="w-full bg-[#8B2323] hover:bg-[#722323] text-white py-2 rounded-md transition duration-300"
-          >
-            UPLOAD FILE
-          </button>
+        <div className="px-6 pt-8 mt-auto bg-black w-full">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Input type="file" className="rounded-full" />
+          </div>
+          <div className="flex justify-end">
+            <button className="bg-[#8B2323] text-white  rounded-3xl px-4 py-2 mt-4">
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </div>
