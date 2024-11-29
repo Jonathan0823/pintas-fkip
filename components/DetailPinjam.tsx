dda9805ab6a1776f9c40a0420c0ddb55ac0759ef
Guinn <eguinjonathan08@gmail.com>
2024-11-29 13:22:22 +0700

    Enhance DetailPinjam component layout and styling; update background color, add header with event details, and improve form structure

--- components/DetailPinjam.tsx
+++ components/DetailPinjam.tsx
@@ -13,17 +13,23 @@ export default async function DetailPinjam({
   console.log(data);
 
   return (
-    <div className="absolute font-sans z-50 inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-      <div className="min-h-screen bg-[#fbf5f0] flex flex-col max-w-md w-full items-center ">
-        <div className="text-[#997c5c] text-3xl md:text-4xl absolute top-1 left-2 hover:cursor-pointer">
-          <Link href="/process">
-            <IoMdArrowRoundBack />
-          </Link>
-        </div>
-
+    <div className="absolute font-sans z-50 inset-0 bg-black/20 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
+      <div className="min-h-screen  flex flex-col max-w-md w-full items-center ">
         <div className="md:w-96 w-80 mt-11 bg-white rounded-2xl border-2 border-black border-dashed flex flex-col max-w-md mx-4 shadow-lg">
-          <div className="p-6 text-xs text-[#8B2323] font-bold">
-            <form className="space-y-2 md:space-y-4">
+          <div className="p-6 relative text-xs text-[#8B2323] font-bold">
+            <div className="text-[#997c5c] text-md md:text-lg absolute top-1 left-2 hover:cursor-pointer">
+              <Link href="/process">
+                <IoMdArrowRoundBack />
+              </Link>
+            </div>
+            <div className="border-b-2 border-black flex justify-between">
+              <div>
+                <h1 className="text-black text-xl">{data?.namaOrmawa}</h1>
+                <h2 className="ml-4">{data?.namaKegiatan}</h2>
+              </div>
+              {data?.startDate?.toISOString().split("T")[0]}
+            </div>
+            <form className="space-y-2 md:space-y-4 mt-2">
               <div className="flex">
                 <label className="w-1/3 text-[#8B2323]">Nama</label>
                 <span className="w-8 text-center">:</span>
@@ -95,7 +101,14 @@ export default async function DetailPinjam({
                       disabled
                     />
                   </div>
+
                 </div>
+                  <div className="border-t-2 border-b-2 border-black mt-2">
+                        test
+                  </div>
+                  <div>
+                    
+                  </div>
               </div>
             </form>
           </div>
